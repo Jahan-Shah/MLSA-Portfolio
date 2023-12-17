9d176b7ebbe13a97ef4f9087d51f53b93d6ec2ef
Shahjahan Sarfraz <110630702+Jahan-Shah@users.noreply.github.com>
2023-12-17 11:36:51 +0000

    portfolio setup

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shah Jahan",
+  title: "Front End Web Developer",
+  email: "jahan.shah.dev@gmail.co",
+  gitHub: "jahan-shah",
+  instagram: "_.jahan_shah",
+  linkedIn: "jahan--shah",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
